6feba8a21be94d09ef1e6d616c8f200e3c626076
Dzenkinstone <kozlovdanil6@gmail.com>
2023-05-24 18:20:16 +0300

    Update isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -3,7 +3,7 @@ const { isValidObjectId } = require("mongoose");
 const isValidId = (req, res, next) => {
   const { petId, noticeId } = req.params;
 
-  const id = noticeId ?? noticeId;
+  const id = petId ?? noticeId;
 
   if (!isValidObjectId(id)) {
     next(res.json(400, { message: `${id} is not valid id` }));
